ba4e46f05aa01aa3aca7ef2fc0592c4e1ef6df60
Magnus Gule <magnus.gule@piscada.com>
2018-11-16 16:17:04 +0100

    pip install coreapi (+pip yaml)

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -7,46 +7,16 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from snippets import views
 
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='Pastebin API')
+
 # Create a router and register our viewsets with it
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
+    path('schema/', schema_view),
     path('', include(router.urls)),
 ]
-
-
-# snippet_list = SnippetViewSet.as_view({
-#     'get': 'list',
-#     'post': 'create'
-# })
-
-# snippet_detail = SnippetViewSet.as_view({
-#     'get': 'retrieve',
-#     'put': 'update',
-#     'patch': 'partial_update',
-#     'delete': 'destroy'
-# })
-
-# snippet_highlight = SnippetViewSet.as_view({
-#     'get': 'highlight'
-# }, renderer_classes=[renderers.StaticHTMLRenderer])
-
-# user_list = UserViewSet.as_view({
-#     'get': 'list'
-# })
-# user_detail = UserViewSet.as_view({
-#     'get': 'retrieve'
-# })
-
-# API endpoints
-# urlpatterns = format_suffix_patterns([
-#     path('', api_root),
-#     path('snippets/', snippet_list, name='snippet-list'),
-#     path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
-#     path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
-#     path('users/', user_list, name='user-list'),
-#     path('users/<int:pk>/', user_detail, name='user-detail'),
-# ])
-
